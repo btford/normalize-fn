e5cc44fec18f5b104e266677d4702f59fbfee040
Brian Ford <btford@umich.edu>
2014-04-02 23:39:28 -0700

    chore: formatting

--- test/normalize.spec.js
+++ test/normalize.spec.js
@@ -2,10 +2,11 @@ var normalize = require('../normalize');
 
 describe('normalize', function () {
   it('should work with funny spaces', function () {
-    expect(normalize('var x    =     y;')).toBe('var x = y;')
+    expect(normalize('var x    =     y;')).toBe('var x = y;');
   });
 
   it('should work with functions', function () {
-    expect(normalize(function foo () {var x = y;})).toBe('function foo() {\n  var x = y;\n}')
+    expect(normalize(function foo () {var x = y;})).
+        toBe('function foo() {\n  var x = y;\n}');
   });
 });
